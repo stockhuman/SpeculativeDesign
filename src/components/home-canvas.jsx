6152e0f74dbb19866a355ed18b60bb375a7949c2
Michael Hemingway <mikemingway@gmail.com>
2018-06-25 17:00:45 -0400

    Working Canvas home component
    Removed pixi and went with a native component. Also optimised the react ref, as well as fixed most of the odd '_this2.xx' is not a function errors created by fn scoping errors on load.

--- src/components/home-canvas.jsx
+++ src/components/home-canvas.jsx
@@ -1,66 +1,238 @@
+'use strict';
+
 import React, { Component } from 'react'
 
 // Images for the mousemove canvas
-import img from '../assets/giphy.gif'
-
-import '../scss/pages/_home.scss'
-
+import img1 from '../assets/home/1.png'
+import img2 from '../assets/home/2.png'
+import img3 from '../assets/home/3.png'
+import img4 from '../assets/home/4.png'
+import img5 from '../assets/home/5.png'
+import img6 from '../assets/home/6.png'
 
 class HomeCanvas extends Component {
 
 	getRandomColor () {
 		const dopecolors = [
-			0x9CBFF3,
-			0xE88484,
-			0x55D6D2
+			'FACACO',
+			'F3C9DD',
+			'OBBCD6',
+			'A6CFE2',
+			'F1C3B8'
 		]
-		return dopecolors[Math.floor(Math.random() * (dopecolors.length -1))]
+
+		return '#' + dopecolors[Math.floor(Math.random() * (dopecolors.length))]
 	}
 
 	init () {
-		if (typeof window !== 'undefined') {
-			const PIXI = require('pixi.js')
-		}
-		const canvas = this.canvas
-		const width = document.documentElement.clientWidth
-		const height = document.documentElement.clientHeight
+		const canvas = this.refs.canvas
+					canvas.width = window.innerWidth
+					canvas.height = window.innerHeight
+					canvas.style.width = canvas.width + 'px'
+					canvas.style.height = canvas.height + 'px'
 
-		const cursor = {
-			x: null,
-			y: null
-		}
-
-		const app = new PIXI.Application(width, height, {
-			resolution: 2,
-			autoResize: true,
-			backgroundColor: this.getRandomColor()
-		})
-		canvas.appendChild(app.view)
-
-		const sprite = PIXI.Sprite.fromImage(img)
-		sprite.anchor.set(0.5)
-		sprite.x = app.screen.width / 2
-		sprite.y = app.screen.height / 2
-
-		app.stage.mousemove = () => {
-
+		this.default = {}
+		this.default.mouse = {
+			x: 0,
+			y: 0
 		}
 
-		app.stage.addChild(sprite)
+		const c = this // to solve a few context errors with the canvas
 
-		canvas.addEventListener('mousemove', onMouseDown, true)
+		this.default.color = this.getRandomColor()
 
-		function onMouseDown (e) {
-			cursor.x = e.clientX
-			cursor.y = e.clientY
-			const s = PIXI.Sprite.fromImage(img)
-			s.x = cursor.x
-			s.y = cursor.y
-			app.stage.addChild(s)
+		this.context = canvas.getContext('2d')
+		this.context.fillStyle = this.default.color
+		this.context.strokeStyle = this.default.color
+		this.context.lineWidth = 0
+
+		this.lastMousePoint = {
+			x: 0,
+			y: 0
+		}
+		this.oldMousePoint = {
+			x: 0,
+			y: 0
 		}
 
-		// window.addEventListener('resize', app.renderer.resize())
+		this.objectIsDrawn = true
+		this.touchSupported ? (
+			this.mouseDownEvent = 'touchstart',
+		  this.mouseMoveEvent = 'touchmove',
+		  this.mouseUpEvent = 'touchend')
+			: (
+				this.mouseDownEvent = 'mouseenter',
+				this.mouseMoveEvent = 'mousemove',
+				this.mouseUpEvent = 'mouseleave'
+		)
+		this.timer = null
+		this.firstRender = false
+		this.id = null
+
+		this.handlers = {}
+		this.handlers.mousedown = (t) => {
+			return onCanvasMouseDown(t)
+		}
+		this.handlers.mouseup = (t) => {
+			return onCanvasMouseUp(t)
+		}
+		this.handlers.mousemove = (t) => {
+			return onCanvasMouseMove(t)
+		}
+		this.handlers.click = (t) => {
+			return updateImage(t)
+		}
+		this.handlers.resize = () => {
+			clear()
+			canvas.width = window.innerWidth
+			canvas.height = window.innerHeight
+			canvas.style.width = canvas.width + 'px'
+			canvas.style.height = canvas.height + 'px'
+			this.context.fillStyle = this.default.color
+			this.context.fillRect(0, 0, canvas.width, canvas.height) // BG
+		}
+		this.currentIndex = -1
+
+		const assets = [{
+			// airplane
+			url: img1,
+			width: 174,
+			height: 210
+		}, {
+			// Mono orifice
+			url: img2,
+			width: 750,
+			height: 750
+		}, {
+			// Pippin
+			url: img3,
+			width: 370,
+			height: 512
+		}, {
+			// Rilla
+			url: img4,
+			width: 527,
+			height: 552
+		}, {
+			// Pippin
+			url: img5,
+			width: 200,
+			height: 200
+		}, {
+			// Work
+			url: img6,
+			width: 640,
+			height: 640
+		}]
+
+		this.currentImageHeight = 0
+		this.currentImageWidth = 0
+
+		this.handlers.requestAnimFrame = () => {
+			return updateCanvasByBrush()
+		}
+
+		const distanceBetween2Points =  function(t, e) {
+      var n = e.x - t.x,
+          r = e.y - t.y;
+      return Math.sqrt(Math.pow(n, 2) + Math.pow(r, 2))
+    }
+
+    const angleBetween2Points = function(t, e) {
+      var n = e.x - t.x,
+          r = e.y - t.y;
+      return Math.atan2(n, r)
+    }
+
+    // Changes the current image from the assets array
+		const updateImage = () => {
+			this.currentIndex = (this.currentIndex >= assets.length - 1) ? 0 : this.currentIndex += 1
+
+			let e = assets[this.currentIndex]
+			this.currentImage = document.createElement('img')
+			let n = this.windowWidth > 0 ? this.windowWidth : window.innerWidth;
+			let r = Math.min(n / 2000, 1);
+
+			this.currentImage.width = e.width * r
+			this.currentImage.height = e.height * r
+			this.currentImage.src = e.url
+		}
+
+		const updateMousePosition = (ctx) => {
+			this.lastMousePoint.x =
+				(this.default.mouse.x !== 0) ? this.default.mouse.x : window.innerWidth / 2;
+			this.lastMousePoint.y =
+			 (0 !== this.default.mouse.y) ? this.default.mouse.y : window.innerHeight / 2;
+		}
+
+		const updateOldMousePosition = () => {
+			this.oldMousePoint.x =
+				(0 !== this.default.mouse.x) ? this.default.mouse.x : window.innerWidth / 2;
+			this.oldMousePoint.y =
+				(0 !== this.default.mouse.y) ? this.default.mouse.y : window.innerHeight / 2;
+		}
+
+		const onCanvasMouseMove = (ctx) => {
+			this.default.mouse.x = ctx.clientX
+			this.default.mouse.y = ctx.clientY
+			return updateMousePosition(ctx), true
+		}
+
+		const onCanvasMouseUp = () => {}
+		const onCanvasMouseDown = () => {}
+
+		const updateCanvasByBrush = () => {
+			let oldmp = {
+				x: this.oldMousePoint.x,
+				y: this.oldMousePoint.y
+			}
+			let lastmp = {
+				x: this.lastMousePoint.x,
+				y: this.lastMousePoint.y
+			}
+			this.oldMousePoint.x = this.lastMousePoint.x;
+			this.oldMousePoint.y = this.lastMousePoint.y;
+
+
+			for (
+				let i = parseInt(distanceBetween2Points(oldmp, lastmp)),
+				o = angleBetween2Points(oldmp, lastmp),
+				s = undefined,
+				u = undefined,
+				l = 0;
+
+					l <= i || 0 === l; l += 1) {
+
+				let dx = oldmp.x + Math.sin(o) * l - this.currentImage.width / 2;
+				let dy = oldmp.y + Math.cos(o) * l - this.currentImage.height / 2;
+
+				// c is 'this' from the init() context
+				c.context.drawImage(this.currentImage, dx, dy, this.currentImage.width, this.currentImage.height);
+
+
+			}
+			this.id = window.requestAnimationFrame(this.handlers.requestAnimFrame)
+		}
+
+		const clear = () => {
+			this.context.clearRect(0, 0, canvas.width, canvas.height)
+		}
+
+		updateImage() // set the first image and params
+
+		// start capturing events
+		setTimeout( () => {
+			this.context.fillStyle = this.default.color
+			this.context.fillRect(0, 0, canvas.width, canvas.height) // BG
+			updateOldMousePosition()
+			updateMousePosition()
+			updateCanvasByBrush()
+			document.addEventListener('mousemove', this.handlers.mousemove)
+			window.addEventListener('resize', this.handlers.resize)
+			document.addEventListener('click', this.handlers.click)
+		}, 0)
 	}
+
 
 	// begin!
 	componentDidMount() {
@@ -69,9 +241,7 @@ class HomeCanvas extends Component {
 
 	render () {
 		return (
-			<div id="home-canvas">
-				<div ref={(canvas) => (this.canvas = canvas)} />
-			</div>
+			<canvas id="home-canvas" ref='canvas' />
 		)
 	}
 }
