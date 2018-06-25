e343bafc33863b370ff821abb46a7712abe5f5db
Michael Hemingway <mikemingway@gmail.com>
2018-06-25 16:57:57 -0400

    canvas graphics

---
+++ src/components/fonts.js
@@ -0,0 +1,17 @@
+// I know, this isn't really a component..
+
+// via https://github.com/gatsbyjs/gatsby/issues/2583
+import MaisonNeueMonoWOFF2 from '../assets/fonts/MaisonNeue-Mono.woff2'
+import MaisonNeueMonoWOFF from '../assets/fonts/MaisonNeue-Mono.woff';
+import MaisonNeueMonoTTF from '../assets/fonts/MaisonNeue-Mono.ttf';
+import MaisonNeueMonoItalicWOFF2 from '../assets/fonts/MaisonNeue-MonoItalic.woff2';
+import MaisonNeueMonoItalicTTF from '../assets/fonts/MaisonNeue-MonoItalic.ttf';
+
+export default {
+	MaisonNeueMonoTTF,
+	MaisonNeueMonoWOFF,
+	MaisonNeueMonoWOFF2,
+
+	MaisonNeueMonoItalicTTF,
+	MaisonNeueMonoItalicWOFF2
+}
