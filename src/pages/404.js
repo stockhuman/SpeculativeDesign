069b917ed1796748844c1d5287f6e17beae41f8f
Michael Hemingway <mikemingway@gmail.com>
2018-06-25 20:00:53 -0400

    New page generator

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,8 @@
 import React from 'react'
+import PageGenerator from '../components/page-generator'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <PageGenerator />
 )
 
 export default NotFoundPage
