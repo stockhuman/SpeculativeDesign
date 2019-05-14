5b5651fe0084ffad70c5f1fe02570c362cd71428
Michael Hemingway <mpghemingway@gmail.com>
2019-05-14 13:21:34 -0400

    Integration of react-three-fiber

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -7,36 +7,49 @@ class Canvas extends Component {
 	constructor(props) {
 		super(props)
 		this.handlers = {}
+		this.canvas = React.createRef()
 
 		this.state = {
 			scene: this.props.scene,
 			dimensions: {x: 0, y: 0},
 			renderer: {
 				antialias: true,
-				alpha: true
+				alpha: true,
+				canvas: null
 			}
 		}
 
-		console.log(this.state)
+
+		this.buildCanvas = this.buildCanvas.bind(this)
 	}
 
-	init () {
+	buildCanvas () {
+		const { canvas, antialias, alpha } = this.state.renderer
+		const renderer = new THREE.WebGLRenderer({ canvas, antialias, alpha })
+		const DPR = window.devicePixelRatio ? window.devicePixelRatio : 1
 
+		renderer.setPixelRatio(DPR);
+		renderer.setSize(this.state.dimensions.x, this.state.dimensions.x)
+
+		renderer.gammaInput = true
+		renderer.gammaOutput = true
+
+		return renderer
 	}
 
-	componentDidMount() {
-		this.canvas = document.getElementById('viewport')
-		this.init()
+	componentDidMount () {
+		this.setState({ renderer: { canvas: this.canvas }})
+		this.buildCanvas()
 	}
 
-	componentWillUnmount() {
+	componentWillUnmount () {
 		// window.removeEventListener('resize', this.handlers.resize)
 		// window.cancelAnimationFrame(this.handlers.requestAnimFrame)
   }
 
 	render () {
 		return (
-			<canvas id="viewport" />
+			<canvas id="viewport" ref={this.canvas} />
 		)
 	}
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@
 import React from 'react'
 
 import HomeCanvas from '../components/canvas/canvas'
-import Caption from '../components/caption'
 import Layout from '../components/layouts/home'
 
 // Styles
@@ -13,14 +12,9 @@ import '../scss/main.scss'
 import '../scss/pages/_home.scss'
 // import Fonts from '../scss/style.js'
 
-
-// const copy = {
-// 	about: 'The Speculative Play project brings together the critical practices and forecasting of speculative design with the hands-on experience of play, and especially the play of interactive digital game-like things. The project operates out of the Technoculture, Art, and Games (TAG) Lab in the Milieux Institute for Arts, Culture, and Technology and the Department of Design and Computation Arts at Concordia University in Montréal and is funded by an FRQSC Team Research-Creation grant.'
-// }
-
 const IndexPage = () => (
   <Layout>
-    <HomeCanvas scene='' />
+    <HomeCanvas scene={{}} />
   </Layout>
 )
 
