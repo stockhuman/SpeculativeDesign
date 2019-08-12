b2f591b59b96a67577ee5cbc4106b2b192ced5a8
Michael Hemingway <mpghemingway@gmail.com>
2019-08-12 15:08:57 -0400

    Misc

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { navigate } from 'gatsby'
 import Link from 'gatsby-link'
 
 import View from '../components/canvas/canvas'
-import Scene from '../components/canvas/scene'
+import Scene from '../components/canvas/Model'
 
 import Layout from '../components/layouts/page'
 
@@ -13,17 +12,10 @@ const About = () => (
 		<Link to='people'><h3>People</h3></Link>
 		<Link to='projects'><h3>Projects</h3></Link>
 
-
-			<View center={[0,0,6]}>
-				<mesh
-					position={[0, 1, -4]}
-					onClick={() => navigate("/about/")}>
-					<planeBufferGeometry attach="geometry" args={[2, 2]} />
-					<meshLambertMaterial attach="material" transparent />
-				</mesh>
-				<Scene url={'meshes/SP_index5.glb'} />
-				<spotLight intensity={0.2} position={[0, 2, 10]} angle={0.4} penumbra={1} castShadow />
-			</View>
+		<View center={[0,0,0]}>
+			<Scene url={'meshes/icosphere.glb'} material />
+			<spotLight intensity={0.2} position={[0, 2, 10]} angle={0.4} penumbra={1} castShadow />
+		</View>
 	</Layout>
 )
 
