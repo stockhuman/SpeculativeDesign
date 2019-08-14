29eb6531adf6bdbd8948d162ef276db0241f50be
Michael Hemingway <mpghemingway@gmail.com>
2019-08-14 09:30:01 -0400

    A more useful 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Link from 'gatsby-link'
 import Layout from '../components/layouts/page'
 
 const NotFoundPage = () => (
-  <Layout>404!</Layout>
+  <Layout>
+    <h1>404</h1>
+    <p>Seems like the absecence of things is still a constant, no matter the world imagined.</p>
+    <Link to='/'>Go home?</Link>
+  </Layout>
 )
 
 export default NotFoundPage
