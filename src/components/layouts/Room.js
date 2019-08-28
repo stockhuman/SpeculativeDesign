0370b22776c33e9f9731bdde883579b725dbfff2
Michael Hemingway <mpghemingway@gmail.com>
2019-08-28 12:44:05 -0400

    New random room model logic

--- src/components/layouts/Room.js
+++ src/components/layouts/Room.js
@@ -19,26 +19,44 @@ export default ({ data }) => {
 	let q = queries()
 	let e = q.indeces.filter(val => val.node.path == path)
 
+	// gets the link to previous and next rooms
 	const next = e[0].next.path
 	const prev = e[0].previous.path
+
+	// gets a random room shape
+	const room = data.markdownRemark.frontmatter.room
+		? data.markdownRemark.frontmatter.room
+		: Math.floor(Math.random() * 2)
+
+	let posprev, posnext
+	switch (room) {
+		case 0:
+			posprev = [4, 1, -4]
+			posnext = [0, 1, -4]
+			break;
+		case 1:
+			posprev = [4, 1, -4]
+			posnext = [0, 1, -4]
+			break;
+	}
 
 	return (
 		<View center={[0, 0, 0]}>
 			<mesh
-				position={[0, 1, -4]}
+				position={posnext}
 				onClick={() => navigate(next)}>
 				<planeBufferGeometry attach="geometry" args={[2, 2]} />
 				{/* Note the invisible element is only hidden from the camera,
 				it is still clickable. */}
-				<meshLambertMaterial attach="material" visible={false} />
+				<meshLambertMaterial attach="material" visible={true} />
 			</mesh>
 			<mesh
-				position={[4, 1, -4]}
+				position={posprev}
 				onClick={() => navigate(prev)}>
 				<planeBufferGeometry attach="geometry" args={[2, 2]} />
 				<meshLambertMaterial attach="material" transparent />
 			</mesh>
-			<Model url={'/meshes/room0.glb'} />
+			<Model url={`/meshes/room${room}.glb`} />
 			<ambientLight intensity={0.01} />
 			<directionalLight intensity={0.5} position={[-1, 1, -1]} />
 			<Sculpture />
