abc9f114f9d144e85d344d26abc389d56478b8ab
Michael Hemingway <mpghemingway@gmail.com>
2019-11-17 16:51:08 -0500

    Documentation

--- src/components/data/queries.js
+++ src/components/data/queries.js
@@ -1,5 +1,6 @@
 import { useStaticQuery, graphql } from "gatsby"
 
+// In a dev environment, this can be inspected @ http://localhost:8000/___graphql
 export default () => {
 	const data = useStaticQuery(graphql`
     query Pages {
