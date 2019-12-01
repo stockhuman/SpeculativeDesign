d4b3baf24c487cc48e4cce3eb745fc058b790f10
Michael Hemingway <mpghemingway@gmail.com>
2019-11-30 22:20:09 -0500

    Implmented user input in log

--- src/components/log.js
+++ src/components/log.js
@@ -1,48 +1,93 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
+import localforage from 'localforage'
 
+import { randomCaption, commands } from './data/strings'
 
-import randomCaption from './data/captions'
 
 class Log extends Component {
 	constructor (props) {
 		super(props)
-		this.data = randomCaption()
+		this.inputNode = createRef()
+		this.timerHandle = null
 		this.state = {
-			copy: '',
-			userInput: '',
-			locked: true
+			terminalOutput: props.intro || randomCaption(), // what the terminal will write on load
+			logs: [], // holds entire conversation log as [{system: bool, copy: string}]
+			locked: true, // can the user type?
 		}
+	}
+
+	addLog = log => {
+		// fancy way of pushing to array without direct mutations
+		// via https://www.robinwieruch.de/react-state-array-add-update-remove
+		this.setState(state => {
+			const logs = [...state.logs, log]
+			return { logs }
+		})
 	}
 
 	// writes one character and calls itself until the text is done
-	tick(text, i) {
+	tick = (text, i) => {
 		if (i < (text.length)) {
-			this.setState({ copy: text.substring(0, i + 1) })
+			this.setState({ terminalOutput: text.substring(0, i + 1) })
 
 			this.timerHandle = setTimeout(() => {
 				this.tick(text, i + 1)
-			}, this.props.interval || 100)
+			}, this.props.interval || 80)
 		} else {
-			this.setState({locked: false})
+			this.addLog({system: true, copy: this.state.terminalOutput})
+			this.setState({locked: false, terminalOutput: ''})
 		}
 	}
 
-	componentDidMount() {
-		this.tick(this.data, 0)
+	// invokes responses based on particular input
+	parseCommand = command => {
+		this.addLog({system:false, copy: command})
+		this.inputNode.current.innerText = ''
+		// if the returned string is not null, it's a valid command.
+		if (commands(command) != null) {
+			this.setState({ terminalOutput: commands(command) })
+			this.tick(this.state.terminalOutput, 0)
+		}
 	}
 
-	componentWillUnmount() {
+	type = e => {
+		if (!this.state.locked) {
+			if (e.key == 'Enter') {
+				e.preventDefault()
+				this.parseCommand(this.inputNode.current.innerText)
+			}
+		} else {
+			e.preventDefault()
+		}
+	}
+
+	componentDidMount = () => {
+		this.tick(this.state.terminalOutput, 0)
+		this.inputNode.current.addEventListener('keydown', this.type)
+	}
+
+	componentWillUnmount = () => {
 		if (this.timerHandle) {
 			clearTimeout(this.timerHandle)
 		}
+		this.inputNode.current.removeEventListener('keydown', this.type)
 	}
 
 	render() {
+		const items = this.state.logs.map((item, key) =>
+			<p key={key}>{item.system ? '' : '> '}{item.copy}</p>
+		)
+
 		return (
 			<div className="crt">
-				<p>> {this.state.copy}</p>
-				<span id='user-input'>{this.state.locked ? null : '> ' + this.state.userInput}</span>
-				<span className="blinking-cursor">{this.state.locked ? null : '|'}</span>
+				{items}
+				<p>{this.state.terminalOutput}</p>
+				<div
+					id='user-input'
+					contentEditable autoFocus
+					ref={this.inputNode}
+					disabled={this.state.locked ? true : false}
+					 />
 			</div>
 		)
 	}
