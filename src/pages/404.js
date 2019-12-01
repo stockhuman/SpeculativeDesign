ab88abdfc93dac764a8a797a9959577226aadad1
Michael Hemingway <mpghemingway@gmail.com>
2019-12-01 14:33:45 -0500

    Themed 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,38 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import { navigate } from 'gatsby'
+
 import Layout from '../components/layouts/page'
+import Sidebar from '../components/layouts/Sidebar'
+import Text from '../components/canvas/Text'
+import View from '../components/canvas/canvas'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404</h1>
-    <p>Seems like the absecence of things is still a constant, no matter the world imagined.</p>
-    <Link to='/'>Go home?</Link>
+    <View center={[0, 0, 0]} background={'#000'}>
+      <mesh onClick={() => navigate('/')} position={[0,0,0]}>
+        <planeBufferGeometry args={[20, 20]} attach="geometry"/>
+        <meshBasicMaterial attach="material" visible={false} />
+      </mesh>
+      <directionalLight intensity={2} position={[0, 2, 2]} />
+      <Text
+        string={'?'}
+        options={
+          {
+            position: [0, 0, 0],
+            color: 'white'
+          }
+        }
+        size={2 + (Math.random() / 1.5)}
+        bevelEnabled={true}
+        height={0.1}
+        bevelThickness={0.2}
+        bevelSize={0.01}
+        bevelSegments={4}
+        curveSegments={8}
+
+      />
+    </View>
+    <Sidebar intro={'Seems like the absecence of things is still a constant, no matter the world imagined. [404]'}/>
   </Layout>
 )
 
