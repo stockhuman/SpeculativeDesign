59c28d6f42844a78780eb526efeb35f4db13f14c
Michael Hemingway <mpghemingway@gmail.com>
2019-12-01 23:05:24 -0500

    Use multiple intro texts in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -32,7 +32,7 @@ const NotFoundPage = () => (
 
       />
     </View>
-    <Sidebar intro={'Seems like the abscence of things is still a constant, no matter the world imagined. [404]'}/>
+    <Sidebar intro={['404','Seems like the abscence of things is still a constant, no matter the world imagined.']}/>
   </Layout>
 )
 
