770f2ac1bec507d0800b7b94cd56b2cd052c3800
Michael Hemingway <mpghemingway@gmail.com>
2020-01-01 22:49:53 -0500

    Created RoomLoader with improved scene parsing and picture frames

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,140 +1,31 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
-import '../scss/layout/_projects.scss'
+// Page Structure
+import View from '../components/canvas/canvas'
+import Model from '../components/canvas/RoomLoader'
+import Layout from '../components/layouts/Page'
+import Sidebar from '../components/layouts/Sidebar'
 
-// Project Images
-import love1 from '../assets/img/love/about.png'
-import love2 from '../assets/img/love/desktop.png'
-import love3 from '../assets/img/love/petite-mort.png'
-import love4 from '../assets/img/love/play.png'
-import love5 from '../assets/img/love/slow-down.png'
-import love6 from '../assets/img/love/title.png'
+// Styles
+import '../scss/main.scss'
+import '../scss/layout/_base.scss'
 
-import work1 from '../assets/img/work/About.png'
-import work2 from '../assets/img/work/Break.png'
-import work3 from '../assets/img/work/Login.png'
-import work4 from '../assets/img/work/Music.png'
-import work5 from '../assets/img/work/Promotion.png'
-import work6 from '../assets/img/work/Working_1.png'
-import work7 from '../assets/img/work/Working_2.png'
+// As the user approaches the website, an imposing museum facade looms in the index..
 
-import matches1 from '../assets/img/matches/Chess-UI.png'
-import matches2 from '../assets/img/matches/Corridor.png'
-import matches3 from '../assets/img/matches/Ramp.png'
-import matches4 from '../assets/img/matches/Room2.png'
-import matches5 from '../assets/img/matches/Snow.png'
-import matches6 from '../assets/img/matches/Stairs.png'
-import matches7 from '../assets/img/matches/UI.jpg'
-import matches8 from '../assets/img/matches/Room.png'
-
-import neoqab1 from '../assets/img/neoqab/Neoqab.jpg'
-import neoqab2 from '../assets/img/neoqab/Neoqab2.png'
-
-import drift1 from '../assets/img/drift/drift.jpg'
-
-import jam1 from '../assets/img/jam/deeptimejam.png'
-
-import orifice0 from '../assets/img/orifice/0.png'
-
-const projects = [
-	{
-		img1: love1,
-		img2: love2,
-		img3: love3,
-		img4: love4,
-		img5: love5,
-		img6: love6,
-		img7: love6,
-		img8: love4,
-		name: 'Love',
-		title: 'It is as if you were making love',
-		link: '/projects/love',
-		wallColor: 'FFEFE5'
-	},
-	{
-		img1: work1,
-		img2: work2,
-		img3: work3,
-		img4: work4,
-		img5: work5,
-		img6: work6,
-		img7: work7,
-		img8: work2,
-		name: 'Work',
-		title: 'It is as if you were doing work',
-		link: '/projects/it-is-as-if-you-were-doing-work',
-		wallColor: 'A7BBC3'
-	},
-	{
-		img1: drift1,
-		img2: '',
-		img3: '',
-		img4: '',
-		img5: '',
-		img6: '',
-		img7: '',
-		img8: '',
-		name: 'DRIFT',
-		title: 'DRIFT workshop',
-		link: 'projects/drift-workshop'
-	},
-	{
-		img1: '',
-		img2: orifice0,
-		img3: '',
-		img4: '',
-		img5: '',
-		img6: '',
-		img7: '',
-		img8: '',
-		name: 'Mono-Orifice™',
-		title: '',
-		link: '/projects/mono-orifice'
-	},
-	{
-		img1: jam1,
-		img2: '',
-		img3: '',
-		img4: '',
-		img5: '',
-		img6: '',
-		img7: '',
-		img8: '',
-		name: 'Deep Time Jam',
-		title: '',
-		link: '/projects/deep-time-jam'
-	},
-	{
-		img1: neoqab1,
-		img2: neoqab2,
-		img3: '',
-		img4: '',
-		img5: '',
-		img6: '',
-		img7: '',
-		img8: '',
-		name: 'NEO//QAB',
-		title: '',
-		link: '/projects/neoqab'
-	},
-	{
-		img1: matches1,
-		img2: matches2,
-		img3: matches3,
-		img4: matches4,
-		img5: matches5,
-		img6: matches6,
-		img7: matches7,
-		img8: matches8,
-		name: 'Burnt Matches',
-		title: '',
-		link: '/projects/burnt-matches'
-	}
-] // todo => query projects for data
-
-
+// note the path is relative to the 'static' directory
+// at the root of the project. .glTF files (or binary, in this case)
+// are not natively supported by webpack, and cannot be imported as the css above.
 const ProjectsPage = () => (
-	<ProjectsPageWrapper projects={projects} />
+	<Layout>
+		<View center={[0, 1.3, 8]}>
+			<Suspense fallback={null}>
+				<Model url={'meshes/tests/testroom.glb'} />
+			</Suspense>
+			<ambientLight intensity={0.2} />
+			<spotLight intensity={3} position={[3, 4, 10]} angle={2} penumbra={2} />
+		</View>
+		<Sidebar />
+	</Layout>
 )
 
 export default ProjectsPage
