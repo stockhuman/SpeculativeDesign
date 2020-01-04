93b2b4cda5b219775ea6e9af147a9827645bb337
Michael Hemingway <mpghemingway@gmail.com>
2020-01-03 22:53:58 -0500

    Rotating text!

--- src/components/canvas/Text.js
+++ src/components/canvas/Text.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useRender } from 'react-three-fiber'
 import helvetica from 'three/examples/fonts/helvetiker_regular.typeface.json'
 import { Font } from 'three/src/extras/core/Font'
 
 // implements https://threejs.org/docs/#api/en/geometries/TextGeometry
-export default ({ string, options = { position: [0,0,0] }, ...props }) => (
-	<mesh position={ options.position } rotation={ options.rotation || [0,0,0] }>
-		<textGeometry attach="geometry"
-			args={[string, { ...{font: new Font(helvetica)}, ...props}]}
-			onUpdate={geo => geo.center()}/>
-		<meshPhongMaterial attach="material" color={options.color || 'white'} />
-	</mesh>
-)
+export default ({ string, options = { position: [0,0,0] }, ...props }) => {
+	let ref = useRef()
+	let t = Math.random() * 100
+	let speed = options.speed || 0.01
+	useRender(() => {
+		t += speed
+		const s = Math.cos(t)
+		ref.current.rotation.set(s * -2, s, s * 0.05)
+	})
+	return (
+		<mesh ref={ref} position={ options.position } rotation={ options.rotation || [0,0,0] }>
+			<textGeometry attach="geometry"
+				args={[string, { ...{font: new Font(helvetica)}, ...props}]}
+				onUpdate={geo => geo.center()}/>
+			<meshPhongMaterial attach="material" color={options.color || 'white'} />
+		</mesh>
+	)
+}
