395c4eb3f1889df40d4de01d2bdb0595e8f25ef1
Michael Hemingway <mpghemingway@gmail.com>
2020-01-05 14:49:18 -0500

    Social icons in sidebar

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -16,10 +16,20 @@ class Sidebar extends Component {
 			<>
 			<aside id="sidebar" className={this.state.opened ? 'open' : ''}>
 				<Log intro={this.props.intro || null}/>
-				<div></div>
-				<h1 className="title">
-					<Link to="/">Speculative Play</Link>
-				</h1>
+				<div>
+					<h1 className="title">
+						<Link to="/">Speculative Play</Link>
+					</h1>
+					<a href="https://www.facebook.com/profile.php?id=100013471624792">
+						<i class="fa fa-facebook" aria-hidden="true"></i>
+					</a>
+					<a href="https://twitter.com/speculativeplay">
+						<i class="fa fa-twitter" aria-hidden="true"></i>
+					</a>
+					<a href="https://www.instagram.com/speculativeplay/">
+						<i class="fa fa-instagram" aria-hidden="true"></i>
+					</a>
+				</div>
 			</aside>
 			<div id="sidebar-toggle" onClick={()=> this.setState({opened: !this.state.opened})}></div>
 			</>
