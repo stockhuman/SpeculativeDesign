008c1aa992575a89c50d526d9fe8cd0fd90a11aa
Michael Hemingway <mpghemingway@gmail.com>
2020-01-18 18:24:18 -0500

    Defaults and checks on variables

--- src/components/canvas/room/Link.js
+++ src/components/canvas/room/Link.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'
 import { navigate } from 'gatsby'
 
-export default function Portal({ link, obj }) {
+import { PlaneBufferGeometry } from 'three'
+
+export default function Portal({ link = "/", obj = {}}) {
 	const [visible, setVisibility] = useState(false);
-	obj.material.opacity = 0; // set initial material to be invisible
+	// set initial material to be invisible
+	if (obj && obj.material) obj.material.opacity = 0;
+	const geo = obj.geometry || new PlaneBufferGeometry(2, 2)
+
 	return (
 		<mesh
 			onPointerEnter={() => setVisibility(true)}
 			onPointerLeave={() => setVisibility(false)}
 			onPointerDown={() => navigate(link)}
-			position={obj.position}
-			geometry={obj.geometry}
-			scale={obj.scale}>
+			position={obj.position || [0,0,0]}
+			geometry={geo}
+			scale={obj.scale || 1}>
 			<meshNormalMaterial attach="material" visible={visible} />
 		</mesh>
 	)
