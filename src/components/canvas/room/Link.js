3dbfc368726b4eb5a633a59a975475e70a4c403f
Michael Hemingway <mpghemingway@gmail.com>
2020-01-19 21:46:00 -0500

    Display url on hover & highlight as wireframe

--- src/components/canvas/room/Link.js
+++ src/components/canvas/room/Link.js
@@ -1,23 +1,42 @@
 import React, { useState } from 'react'
 import { navigate } from 'gatsby'
-
-import { PlaneBufferGeometry } from 'three'
+import { useThree } from 'react-three-fiber'
+import helvetica from 'three/examples/fonts/helvetiker_regular.typeface.json'
+import { Font } from 'three/src/extras/core/Font'
 
 export default function Portal({ link = "/", obj = {}}) {
 	const [visible, setVisibility] = useState(false);
 	// set initial material to be invisible
 	if (obj && obj.material) obj.material.opacity = 0;
-	const geo = obj.geometry || new PlaneBufferGeometry(2, 2)
+	const geo = obj.geometry
+	const { camera } = useThree()
 
 	return (
-		<mesh
-			onPointerEnter={() => setVisibility(true)}
-			onPointerLeave={() => setVisibility(false)}
-			onPointerDown={() => navigate(link)}
-			position={obj.position || [0,0,0]}
-			geometry={geo}
-			scale={obj.scale || 1}>
-			<meshNormalMaterial attach="material" visible={visible} />
-		</mesh>
+		<group>
+			<mesh
+				onPointerEnter={() => setVisibility(true)}
+				onPointerLeave={() => setVisibility(false)}
+				onPointerDown={() => navigate(link)}
+				position={obj.position || [0,0,0]}
+				geometry={geo}
+				scale={obj.scale || 1}>
+				<meshBasicMaterial attach="material" visible={visible} wireframe />
+			</mesh>
+			{visible ?
+				<mesh
+					onUpdate={self => self.lookAt(camera.getWorldPosition())}
+					position={obj.position}
+					rotation={obj.rotation || [0, 0, 0]}>
+					<textGeometry attach="geometry"
+						args={[link, { ...{ font: new Font(helvetica) },
+							size: 0.4,
+							height: 0.2,
+							curveSegments: 2
+						}]}
+						onUpdate={geo => void geo.center() } />
+					<meshNormalMaterial attach="material" />
+				</mesh>
+			: null}
+		</group>
 	)
 }
