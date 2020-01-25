1d78a18e4419bf1c6efcd8b0cd54e3f55e6151d3
Michael Hemingway <mpghemingway@gmail.com>
2020-01-25 17:42:38 -0500

    Switch to useFrame for use in v4

--- src/components/canvas/Text.js
+++ src/components/canvas/Text.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import { useRender } from 'react-three-fiber'
+import { useFrame } from 'react-three-fiber'
 import helvetica from 'three/examples/fonts/helvetiker_regular.typeface.json'
 import { Font } from 'three/src/extras/core/Font'
 
@@ -8,7 +8,7 @@ export default ({ string, options = { position: [0,0,0] }, ...props }) => {
 	let ref = useRef()
 	let t = Math.random() * 100
 	let speed = options.speed || 0.01
-	useRender(() => {
+	useFrame(() => {
 		t += speed
 		const s = Math.cos(t)
 		ref.current.rotation.set(s * -2, 0, s * 0.05)
