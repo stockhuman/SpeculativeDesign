1f3253a18578a526abaff408ffc71feddd352405
Michael Hemingway <mpghemingway@gmail.com>
2020-01-25 15:39:49 -0500

    Migrate projects test page to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,54 +1,32 @@
 import React from 'react'
-import { navigate } from 'gatsby'
 
+// Page Structure
 import View from '../components/canvas/canvas'
-import Model from '../components/canvas/Model'
+import Room from '../components/canvas/room/Room'
 import Layout from '../components/layouts/Page'
 import Sidebar from '../components/layouts/Sidebar'
-import queries from '../components/data/queries'
+
+// Styles
+import '../scss/main.scss'
+import '../scss/layout/_base.scss'
 
 // The about page serves to branch the two paths: one to artists and the other
 // to projects. This interstitial space also serves to describe the project
 
 // Each branch takes one on a successive journey though each room, project or person.
 // the order of each project is determined by alphanumeric sorting, thus the numbered entries.
-const About = () => {
-	// first one (as it is the shallowest in the project tree)
-	const { people, projects } = queries()
-	const firstProject = projects[0].path
-	const firstPerson = people[0].path
-
-	return (
-		<Layout>
-			<View center={[0,0,0]}>
-				<mesh
-					position={[0, 1, -4]}
-					onClick={() => navigate('/bibliography')}>
-					<planeBufferGeometry attach="geometry" args={[2, 2]} />
-					<meshLambertMaterial attach="material" transparent />
-				</mesh>
-				<mesh
-					position={[4, 1, 0]}
-					rotation={[1,-1,1]}
-					onClick={() => navigate(firstProject)}>
-					<planeBufferGeometry attach="geometry" args={[2, 2]} />
-					<meshBasicMaterial attach="material" color="peachpuff" />
-				</mesh>
-				<mesh
-					position={[-4, 1, 0]}
-					rotation={[0,1,0]}
-					onClick={() => navigate(firstPerson)}>
-					<planeBufferGeometry attach="geometry" args={[2, 2]} />
-					<meshLambertMaterial attach="material" color="blue" />
-				</mesh>
-
-				<Model url={'/meshes/about/about2_002.glb'} />
-
-				<directionalLight intensity={2} position={[0, 2, 2]} />
-			</View>
-			<Sidebar intro="Welcome welcome welcome welcome, you!"/>
-		</Layout>
-	)
-}
-
-export default About
+export default () => (
+	<Layout>
+		<View center={[0, 1.3, 8]}>
+			<Room url={'/meshes/tests/about12.glb'} data={{
+				LinkProjects: '/projects/it-is-as-if-you-were-doing-work',
+				LinkPeople: '/people/agustina-isidori',
+				LinkBibliography: '/bibliography'
+			}} />
+			{/* <ambientLight intensity={1} /> */}
+			<directionalLight intensity={0.3} position={[0, 2, 2]} />
+			<spotLight intensity={8} position={[0, 1, 10]} angle={110} penumbra={0.5} castShadow />
+		</View>
+		<Sidebar intro="Welcome, welcome, welcome!"/>
+	</Layout>
+)
