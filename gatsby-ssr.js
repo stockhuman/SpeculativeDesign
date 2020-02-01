73e1eb6e5deb0204585477fa8ed9d09f4fb30699
Michael Hemingway <mpghemingway@gmail.com>
2020-01-31 21:53:48 -0500

    Remove Gatsby SSR onRenederBody call
     in favour of future solution with helment beta

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,34 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
- // Note title is broken - Helmet is unmaintained and Queries need work.
-const React = require('react')
-const { useStaticQuery, graphql } = require('gatsby')
-
-exports.onRenderBody = ({ setHeadComponents }) => {
-	// const data = useStaticQuery(graphql`
-	// 	query PageTitleQuery($path: String!) {
-	// 		markdownRemark(frontmatter: { path: { eq: $path } }) {
-	// 			frontmatter {
-	// 				path
-	// 				title
-	// 			}
-	// 		}
-	// 		site {
-	// 			siteMetadata {
-	// 				title
-	// 			}
-	// 		}
-	// 	}`
-	// )
-
-	// let pageTitle = data.markdownRemark ? data.markdownRemark.frontmatter.title : null
-	// const title = pageTitle ? pageTitle : data.site.siteMetadata.title
-
-	setHeadComponents([
-		// <meta name="title" content={title} />,
-		// <meta name="description" content={''} />,
-		<link key="gfonts" rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Libre+Barcode+128+Text" />
-	])
-}
