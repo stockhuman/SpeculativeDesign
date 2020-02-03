335836d2d9af5f9b530d36ebc06316a99506f585
Michael Hemingway <mpghemingway@gmail.com>
2020-02-02 19:10:33 -0500

    Use DOM instead of 3D rendered text for links

--- src/components/canvas/room/Link.js
+++ src/components/canvas/room/Link.js
@@ -1,43 +1,26 @@
 import React, { useState } from 'react'
 import { navigate } from 'gatsby'
-import { useThree } from 'react-three-fiber'
-import typeface from '../../../assets/fonts/uni-neue-italic.json'
-import { Font } from 'three/src/extras/core/Font'
-import { Vector3 } from 'three'
+import { Dom } from 'react-three-fiber'
 
 export default function Portal({ link = "/", obj = {}}) {
 	const [visible, setVisibility] = useState(false);
 	// set initial material to be invisible
 	if (obj && obj.material) obj.material.opacity = 0;
 	const geo = obj.geometry
-	const { camera } = useThree()
-	const font = new Font(typeface)
 
 	return (
 		<group>
 			<mesh
 				onPointerEnter={() => { setVisibility(true); document.body.style.cursor = 'pointer' }}
 				onPointerLeave={() => { setVisibility(false); document.body.style.cursor = '' }}
-				onPointerDown={() => { document.body.style.cursor = ''; navigate(link)}}
+				onClick={() => { document.body.style.cursor = ''; navigate(link)}}
 				position={obj.position || [0,0,0]}
 				geometry={geo}
 				scale={obj.scale || 1}>
 				<meshBasicMaterial attach="material" visible={visible} wireframe />
 			</mesh>
 			{visible ?
-				<mesh
-					onUpdate={self => self.lookAt(camera.getWorldPosition(new Vector3))}
-					position={obj.position}
-					rotation={obj.rotation || [0, 0, 0]}>
-					<textGeometry attach="geometry"
-						args={[link, { ...{ font },
-							size: 0.4,
-							height: 0.2,
-							curveSegments: 2
-						}]}
-						onUpdate={geo => void geo.center() } />
-					<meshNormalMaterial attach="material" />
-				</mesh>
+				<Dom><span className="canvas-dom-link">Navigate to {link}</span></Dom>
 			: null}
 		</group>
 	)
