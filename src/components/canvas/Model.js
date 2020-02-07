45069d0489fa49f9e86f085d09df9991c0f29861
Michael Hemingway <mpghemingway@gmail.com>
2020-02-07 15:03:00 -0500

    formatting

--- src/components/canvas/Model.js
+++ src/components/canvas/Model.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { withPrefix } from 'gatsby'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { useLoader } from 'react-three-fiber'
+
 /**
  * Drastically simplified Model importer no longer takes custom material parameters
  * @param {string} url The path to a .glb, .gltf file to load
