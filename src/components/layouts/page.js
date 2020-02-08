8116751f450a618f19cef83612af3274b01eae53
Michael Hemingway <mpghemingway@gmail.com>
2020-02-08 13:51:28 -0500

    Meta tags and favicon

--- src/components/layouts/page.js
+++ src/components/layouts/page.js
@@ -4,6 +4,7 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 import Fonts from '../../scss/style'
 import '../../scss/main.scss'
+import ico from '../../assets/logos/favicon.ico'
 
 export default ({ children }) => {
 
@@ -30,11 +31,12 @@ export default ({ children }) => {
   <HelmetProvider>
     <Helmet>
       <meta charset="utf-8" />
-      <meta http-equiv="x-ua-compatible" content="ie=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <meta name="description" content={''} />
+      <meta rel="icon" href={ico} />
       <title>{title}</title>
       <style>{Fonts}</style>
+
     </Helmet>
     {children}
   </HelmetProvider>
