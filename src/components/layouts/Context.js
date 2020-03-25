5daf6117b06df380903f7deaf17db4435d1c02c0
Michael Hemingway <mpghemingway@gmail.com>
2020-03-25 18:45:12 -0400

    Update Context.js

--- src/components/layouts/Context.js
+++ src/components/layouts/Context.js
@@ -1,12 +1,26 @@
-import React, { useState, createContext } from 'react'
+import React, { createContext, useContext, useCallback } from 'react'
+import { useLocalStorage } from '../data/useLocalStorage'
 
 export const nonsenseContext = createContext()
 
-export const nonsenseProvider = props => {
-	const [nonsense, setNonsense] = useState(true)
+export const NonsenseProvider = props => {
+	const [nonsense, setNonsense] = useLocalStorage('nonsense', true)
 	return (
 		<nonsenseContext.Provider value={[nonsense, setNonsense]}>
 			{props.children}
 		</nonsenseContext.Provider>
 	)
+}
+
+export function useNonsense () {
+	const context = useContext(nonsenseContext)
+	const [ nonsenseValue, setNonsense ] = context
+	const toggleNonsense = useCallback(() => {
+		if (nonsenseValue) setNonsense(false)
+		else setNonsense(true)
+	}, [nonsenseValue])
+	return {
+		nonsense: nonsenseValue,
+		toggleNonsense
+	}
 }
