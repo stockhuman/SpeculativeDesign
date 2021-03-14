e7ec3917b3c7e1df569a70bfefbdcbb67210b7f3
Michael Hemingway <mpghemingway@gmail.com>
2021-03-14 14:00:03 -0400

    Use new controls

--- src/components/canvas/canvas.js
+++ src/components/canvas/canvas.js
@@ -1,48 +1,45 @@
-import React, { useRef, Suspense } from 'react'
-import { extend, Canvas, useThree, useFrame } from 'react-three-fiber'
+import React, { Suspense } from 'react'
+import { Canvas } from 'react-three-fiber'
 import { Html, Stars } from '@react-three/drei'
-import { Vector3, sRGBEncoding } from 'three'
+
+import { Controls } from './OrbitControls'
 
 import { loaders } from '../data/strings'
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-extend({ OrbitControls })
-
-
 // This function is abstracted from the view method so as to let size variables instantiate
 // Many performance optimisations derived from https://discoverthreejs.com/tips-and-tricks/
-function Controls (props) {
-	const controls = useRef()
-	const { gl, camera } = useThree()
-	const center = new Vector3(props.center[0], props.center[1], props.center[2])
+// function Controls(props) {
+// 	const ref = useRef()
+// 	const { gl, camera, invalidate } = useThree()
 
-	// see https://codesandbox.io/s/j3yrl1k9rw
-	useFrame(() => (controls.current ? controls.current.update() : null))
+// 	// see https://e0g9z.csb.app/
+// 	useFrame(() => ref.current.update())
+// 	useEffect(() => void ref.current.addEventListener('change', invalidate), [])
 
-	camera.far = props.far || 1000
+// 	camera.far = props.far || 1000
 
-	return (
-		<orbitControls
-			ref={controls}
-			args={[camera, gl.domElement]}
-			enableDamping
-			enableZoom={props.enableZoom || false}
-			enablePan={props.enablePan || false}
-			maxPolarAngle={props.maxPolarAngle || Math.PI / 1.6} // bigger divisor = more you can look up
-			minPolarAngle={props.minPolarAngle || Math.PI / 2.0} // bigger divisor = more you can look down
-			dampingFactor={0.07}
-			target={center}
-			position={props.cameraPlacement}
-			rotateSpeed={0.5}
-		/>
-	)
-}
+// 	return (
+// 		<orbitControls
+// 			ref={ref}
+// 			args={[camera, gl.domElement]}
+// 			enableDamping
+// 			enableZoom={props.enableZoom || false}
+// 			enablePan={props.enablePan || false}
+// 			maxPolarAngle={props.maxPolarAngle || Math.PI / 1.6} // bigger divisor = more you can look up
+// 			minPolarAngle={props.minPolarAngle || Math.PI / 2.0} // bigger divisor = more you can look down
+// 			dampingFactor={0.07}
+// 			target={[props.center[0], props.center[1], props.center[2]]}
+// 			position={props.cameraPlacement}
+// 			rotateSpeed={0.5}
+// 		/>
+// 	)
+// }
 
 export default function View(props) {
-	const center = props.center ? props.center : [0, 0, 0];
+	const center = props.center ? props.center : [0, 0, 0]
 	const cameraPlacement = props.cameraPosition
 		? props.cameraPosition
-		: [1, 0, 2];
+		: [1, 0, 2]
 
 	// this is a hacky fix for gatsby SSR - where window is not available
 	if (typeof window === 'undefined') {
@@ -50,18 +47,18 @@ export default function View(props) {
 	}
 
 	return (
-		<main id='viewport'>
+		<main id="viewport">
 			<Canvas
-				pixelRatio={Math.min(window.devicePixelRatio, 3) || 1}
+				pixelRatio={[1, 3]}
 				onCreated={({ gl }) => {
 					gl.alpha = false
 					gl.antialias = false
 					gl.setClearColor(props.background || '#000000')
-					gl.outputEncoding = sRGBEncoding
+					gl.outputEncoding = 0 // sRGBEncoding
 					gl.physicallyCorrectLights = true
 				}}
 				concurrent
-				>
+			>
 				<Controls
 					center={center}
 					cameraPlacement={cameraPlacement}
@@ -80,16 +77,16 @@ export default function View(props) {
 					castShadow
 				/>
 				<Stars />
-				<Suspense fallback={
-					<Html center
-						className="loader"
-						position={[0, 0, 0]}>
-						<span>{loaders()}</span>
-					</Html>
-					}>
-						{/* <Env /> */}
+				<Suspense
+					fallback={
+						<Html center className="loader" position={[0, 0, 0]}>
+							<span>{loaders()}</span>
+						</Html>
+					}
+				>
+					{/* <Env /> */}
 					{/* <fog attach="fog" args={['#fbf7f5', 16, 80]} /> */}
-					{/* <ambientLight intensity={0.2}/> */}
+					<ambientLight intensity={0.2} />
 					<scene>{props.children}</scene>
 				</Suspense>
 			</Canvas>
