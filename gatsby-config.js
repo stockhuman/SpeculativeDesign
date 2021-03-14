0fca5236af4b8ace2ecb825fd47a3e4a78cc9b25
Michael Hemingway <mpghemingway@gmail.com>
2021-03-14 13:59:39 -0400

    organising plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,13 @@ module.exports = {
     title: 'Speculative Play'
   },
   plugins: [
+		'gatsby-plugin-image',
+		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-sass',
+		'gatsby-plugin-sharp',
 		'gatsby-remark-copy-linked-files',
 		'gatsby-transformer-sharp',
-		'gatsby-plugin-sharp',
-		'gatsby-plugin-react-helmet',
+		'gatsby-transformer-remark',
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
@@ -28,8 +30,7 @@ module.exports = {
 			options: {
 				maxWidth: 1080,
 			},
-		},
-		'gatsby-transformer-remark'
+		}
   ],
 	pathPrefix: `/site/sp` // for live.arthem.co/site/sp
 	// pathPrefix: `/new` // for staging
