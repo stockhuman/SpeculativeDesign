9122d1fcf0e7c11e776f74e47d474eeef04638e0
Michael Hemingway <mpghemingway@gmail.com>
2021-03-27 17:13:28 -0400

    Re-create the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,33 +1,40 @@
 import React from 'react'
 import { navigate } from 'gatsby'
 
-// import Text from '../components/canvas/Text'
-// import View from '../components/canvas/canvas'
+// Page Structure
+import Page from '../templates/layout/Page'
+import HUD from '../templates/layout/HUD'
 
-const NotFoundPage = () => ( <></>
-  // <View center={[0, 0, 0]} background={'#000'}>
-  //   <mesh onClick={() => navigate('/')} position={[0,0,0]}>
-  //     <planeBufferGeometry args={[20, 20]} attach="geometry"/>
-  //     <meshBasicMaterial attach="material" visible={false} />
-  //   </mesh>
-  //   <directionalLight intensity={2} position={[0, 2, 2]} />
-  //   <Text
-  //     string={'?'}
-  //     options={
-  //       {
-  //         position: [0, 0, 0],
-  //         color: 'white'
-  //       }
-  //     }
-  //     size={2 + (Math.random() / 1.5)}
-  //     bevelEnabled={true}
-  //     height={0.1}
-  //     bevelThickness={0.2}
-  //     bevelSize={0.01}
-  //     bevelSegments={4}
-  //     curveSegments={8}
-  //   />
-  // </View>
+import Canvas from '../components/canvas/Canvas'
+import Text from '../components/canvas/Text'
+
+const NotFoundPage = () => (
+	<Page title="404">
+		<main id="viewport">
+			<Canvas center={[0, 0, 0]}>
+				<mesh onClick={() => navigate('/')} position={[0, 0, 0]}>
+					<planeBufferGeometry args={[20, 20]} />
+					<meshBasicMaterial visible={false} />
+				</mesh>
+				<directionalLight intensity={2} position={[0, 2, 2]} />
+				<Text
+					string={'?'}
+					options={{
+						position: [0, 100, -180],
+						color: 'white',
+					}}
+					size={30}
+          height={2}
+					bevelEnabled={true}
+					bevelThickness={3}
+					bevelSize={5}
+					bevelSegments={1}
+					curveSegments={3}
+				/>
+			</Canvas>
+		</main>
+		<HUD />
+	</Page>
 )
 
 export default NotFoundPage
