05e0ec85aa225dcc27d877f4a46600b3bb1628e5
Michael Hemingway <mpghemingway@gmail.com>
2021-03-27 17:12:55 -0400

    Wrap whole page in Suspense to deal with nn hook

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import { NonsenseProvider } from './src/components/hooks/Nonsense'
 
+// necessary as nonSense hook suspends sometimes while querying for the value.
 export const wrapRootElement = ({ element }) => (
-	<NonsenseProvider>{element}</NonsenseProvider>
+	<NonsenseProvider>
+		<Suspense fallback={null}>{element}</Suspense>
+	</NonsenseProvider>
 )
