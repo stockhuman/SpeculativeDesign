12b271f1c40635055824ccddf3607d56cb8cca11
Michael Hemingway <mpghemingway@gmail.com>
2021-03-27 21:48:36 -0400

    Add images to project template

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { graphql } from 'gatsby'
+import { Text } from '@react-three/drei'
 
 import HUD from './layout/HUD'
 import Page from './layout/Page'
 import Viewport from './layout/Viewport'
-import { Text } from '@react-three/drei'
+
+import Cadre from '../components/canvas/Cadre'
 
 function stripHTML(html) {
 	let tmp = document.createElement('div')
@@ -15,20 +17,48 @@ function stripHTML(html) {
 export default function Template({ data }) {
 	const fm = data.markdownRemark.frontmatter
 
-	console.log(data)
+	console.log(data.markdownRemark.frontmatter)
 
 	const html = stripHTML(data.markdownRemark.html)
+
+	const Pictures = () => {
+		let pics = []
+
+		const rand = (max, min) => Math.random() * (max - min) + min
+
+		fm.images.forEach((img, i) => {
+			pics[i] = {
+				src: img,
+				key: i,
+				pos: [rand(-5, 5), rand(-3, 2), -1.5],
+				rot: [rand(-5, 5), rand(-5, 5), 0],
+				animated: true,
+			}
+		})
+
+		console.log(pics)
+
+		return pics.map((pic) => (
+			<Cadre
+				img={pic.src}
+				position={pic.pos}
+				rotation={pic.rot}
+				key={pic.key}
+				animated={pic.animated}
+			/>
+		))
+	}
+
+	const pics = useMemo(() => Pictures(), [])
 
 	return (
 		<Page title={fm.title} description={html}>
 			<Viewport data={data.markdownRemark} images={fm.images}>
-				<Text maxWidth={4} fontSize={0.2}>
-						<meshNormalMaterial
-							depthWrite={false}
-							depthTest={false}
-						/>
+				<Text maxWidth={4.5} fontSize={0.2}>
+					<meshNormalMaterial depthWrite={false} depthTest={false} side={2} />
 					{html}
 				</Text>
+				{pics}
 			</Viewport>
 			<HUD />
 		</Page>
