48e1a7c6d25de8f53a27ecf0393769428d3ba98b
Michael Hemingway <mpghemingway@gmail.com>
2021-03-28 22:51:37 -0400

    Formatting & spin on hover

--- src/components/canvas/Link.js
+++ src/components/canvas/Link.js
@@ -1,68 +1,92 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { navigate, withPrefix } from 'gatsby'
-import { useLoader } from 'react-three-fiber'
-import { Text } from '@react-three/drei'
+import { Text, useGLTF } from '@react-three/drei'
+import { useFrame } from 'react-three-fiber'
 import { MeshBasicMaterial } from 'three/src/materials/MeshBasicMaterial'
-import { GLTFLoader } from 'three-stdlib'
-
-/**
- * Creates a navigable link given a path and 3D object data
- * @param {string} link the page to navigate to
- * @param {object} obj THREE mesh object (or parameters)
- */
-export default function LinkedObject({ link = "/", obj = {}}) {
-	const [visible, setVisibility] = useState(false);
-	// set initial material to be invisible
-	if (obj && obj.material) obj.material.opacity = 0;
-	const geo = obj.geometry
-
-	return (
-		<group>
-			<mesh
-				onPointerEnter={() => { setVisibility(true); document.body.style.cursor = 'pointer' }}
-				onPointerLeave={() => { setVisibility(false); document.body.style.cursor = '' }}
-				onClick={() => { document.body.style.cursor = ''; navigate(link)}}
-				position={obj.position || [0,0,0]}
-				geometry={geo}
-				scale={obj.scale || 1}>
-				<meshBasicMaterial visible={visible} wireframe />
-			</mesh>
-			{visible ?
-				<Text color="white" position={obj.position}>Navigate to {link}</Text>
-			: null}
-		</group>
-	)
-}
-
 /**
  * Creates a navigable link given a path and a GLTF file
  * @param {string} link the page to navigate to
  * @param {object} url The path to a .glb, .gltf file to load
  */
-export function LinkedModel ({ link = '/', url }) {
-	const gltf = useLoader(GLTFLoader, withPrefix(url))
-	const [visible, setVisibility] = useState(false)
+export function LinkedModel({ link = '/', url, ...props }) {
+	const gltf = useGLTF(withPrefix(url))
+	const [hovered, setVisibility] = useState(false)
 
 	const scene = gltf.scene.clone(true)
-	let pos = [0,0,0]
-	scene.traverse(obj => {
+	let pos = [0, 0, 0]
+	scene.traverse((obj) => {
 		let color = obj.material ? obj.material.color : 'yellow'
-		obj.material = new MeshBasicMaterial({wireframe: true, visible, color})
+		obj.material = new MeshBasicMaterial({ wireframe: true, visible: hovered, color })
 		pos = obj.position
+	})
+
+	const group = useRef()
+	useFrame(() => {
+		group.current && hovered ? (group.current.rotation.y += 0.05) : null
 	})
 
 	return (
 		<group
-			onPointerEnter={() => { setVisibility(true); document.body.style.cursor = 'pointer' }}
-			onPointerLeave={() => { setVisibility(false); document.body.style.cursor = '' }}
-			onClick={() => { document.body.style.cursor = ''; navigate(link) }}
+			{...props}
+			ref={group}
+			onPointerEnter={() => {
+				setVisibility(true)
+				document.body.style.cursor = 'pointer'
+			}}
+			onPointerLeave={() => {
+				setVisibility(false)
+				document.body.style.cursor = ''
+			}}
+			onClick={() => {
+				document.body.style.cursor = ''
+				navigate(link)
+			}}
 		>
-			<primitive object={scene}/>
+			<primitive object={gltf.scene} />
+			<primitive object={scene} />
 
-			{visible ?
-				<Text color="white" position={pos}>Navigate to {link}</Text>
-				: null}
+			{hovered ? (
+				<Text color="white" position={pos}>
+					Navigate to {link}
+				</Text>
+			) : null}
 		</group>
 	)
-
 }
+
+export function Door(props) {
+	const { nodes } = useGLTF(withPrefix('/models/door.glb'))
+	const [hovered, setVisibility] = useState(false)
+	return (
+		<group
+			{...props}
+			onPointerEnter={() => {
+				setVisibility(true)
+				document.body.style.cursor = 'pointer'
+			}}
+			onPointerLeave={() => {
+				setVisibility(false)
+				document.body.style.cursor = ''
+			}}
+			onClick={() => {
+				document.body.style.cursor = ''
+				navigate(props.link)
+			}}
+		>
+			<mesh geometry={nodes.DoorFrame.geometry}>
+				<meshStandardMaterial color="#302122" roughness={0.7} />
+			</mesh>
+			<mesh geometry={nodes.Door.geometry}>
+				{hovered ? (
+					<meshStandardMaterial color="#d2e63e" />
+				) : (
+					<meshNormalMaterial />
+				)}
+			</mesh>
+			<mesh geometry={nodes.DoorFace.geometry} />
+			{hovered ? <Text color="white">Navigate to {props.link}</Text> : null}
+		</group>
+	)
+}
+
+useGLTF.preload(withPrefix('/door.glb'))
