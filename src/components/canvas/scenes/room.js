cef1f2036721cfc1c9dde26bfdaad1e4e03898dd
Michael Hemingway <mpghemingway@gmail.com>
2021-03-28 23:04:51 -0400

    Reposition doors in template

--- src/components/canvas/scenes/room.js
+++ src/components/canvas/scenes/room.js
@@ -3,20 +3,28 @@ import { Door } from '../Link'
 
 // Sets up walls and links to adjacent rooms in people & project templates
 export default function Room({ to, from, alt }) {
-
 	return (
 		<group>
-			{/* <mesh>
-				<planeBufferGeometry args={[10, 10]} />
-			</mesh> */}
-			<mesh position={[-5, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
+			{/* <mesh position={[-5, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
 				<planeBufferGeometry args={[100, 100]} />
-			</mesh>
+			</mesh> */}
 			<fog color={'#111'} />
-			<ambientLight intensity={0.6} />
-			{to ? <Door link={to} position={[1, 1, 1]} /> : null}
-			{from ? <Door link={from} /> : null}
-			{alt ? <Door link={alt} /> : null}
+			<ambientLight intensity={0.8} />
+			{to ? (
+				<Door
+					link={to}
+					position={[-3, 2, 1]}
+					rotation={[0.07, Math.PI / 3, 0.03]}
+				/>
+			) : null}
+			{from ? (
+				<Door
+					link={from}
+					position={[3, 1, 2]}
+					rotation={[0.07, -Math.PI / 3.1, 0.03]}
+				/>
+			) : null}
+			{alt ? <Door link={alt} position={[4, 4, 1]} /> : null}
 		</group>
 	)
 }
