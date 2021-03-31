911eef55646ab93bff16e9d78937fc6bfeb879c1
Michael Hemingway <mpghemingway@gmail.com>
2021-03-30 22:47:59 -0400

    Reimplement links to projects and people

--- src/components/canvas/scenes/about.js
+++ src/components/canvas/scenes/about.js
@@ -11,6 +11,25 @@ export default function AboutScene() {
 				position={[4, 2, -3]}
 				rotation={[-0.5, 0.3, 0.4]}
 			/>
+			<LinkedModel
+				link={'/people/agustina-isidori'}
+				url={'/models/bust.glb'}
+				position={[-5, 2, -2]}
+				rotation={[0, Math.PI / 2, 0]}
+			/>
+			<spotLight
+				intensity={6}
+				position={[-4, 6, -3]}
+				penumbra={0.4}
+				angle={1}
+			/>
+			<pointLight position={[-5, 2, -2]} intensity={2} />
+			<LinkedModel
+				link={'/projects/it-is-as-if-you-were-doing-work'}
+				url={'/models/door.glb'}
+				position={[0, 1, -2]}
+				rotation={[0, Math.PI / 2, 0]}
+			/>
 		</>
 	)
 }
