40072b197d95140516d07428e2b96c1176a87ce1
Michael Hemingway <mpghemingway@gmail.com>
2021-04-11 19:32:03 -0400

    Split About page text into two columns

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,15 +18,15 @@ import { Text } from '@react-three/drei'
 
 // Each branch takes one on a successive journey though each room, project or person.
 // the order of each project is determined by alphanumeric sorting, thus the numbered entries.
-const aboutText = `The Speculative Play project brings together the critical practices and forecasting of speculative design with the hands-on experience of play, and especially the play of interactive digital game-like things. The project operates out of the Technoculture, Art, and Games (TAG) Lab in the Milieux Institute for Arts, Culture, and Technology and the Department of Design and Computation Arts at Concordia University in Montréal and is funded by an FRQSC Team Research-Creation grant.\n
+const aboutText = [`The Speculative Play project brings together the critical practices and forecasting of speculative design with the hands-on experience of play, and especially the play of interactive digital game-like things. The project operates out of the Technoculture, Art, and Games (TAG) Lab in the Milieux Institute for Arts, Culture, and Technology and the Department of Design and Computation Arts at Concordia University in Montréal and is funded by an FRQSC Team Research-Creation grant.\n
 
-Speculative design, related to design stances such as critical design and design fiction, takes the position that design can prompt speculation on alternative presents and futures. Speculative design re-imagines invisible and embedded cultural assumptions of how the world is and proposes instead, “How the world could be”, and “Why isn’t the world like this?” Many examples of speculative design concern physical objects, sometimes accompanied by adept social engineering to make them appear real. If speculative design is about inviting people to entertain alternative realities, however, we should consider how speculation is enacted and think through concerns around accessibility of speculative projects.\n
+Speculative design, related to design stances such as critical design and design fiction, takes the position that design can prompt speculation on alternative presents and futures. Speculative design re-imagines invisible and embedded cultural assumptions of how the world is and proposes instead, “How the world could be”, and “Why isn’t the world like this?” Many examples of speculative design concern physical objects, sometimes accompanied by adept social engineering to make them appear real. If speculative design is about inviting people to entertain alternative realities, however, we should consider how speculation is enacted and think through concerns around accessibility of speculative projects.\n`,
 
-Play has traditionally been one way in which people have felt comfortable in performing speculation and transgressing their day-to-day roles. Interactivity can support the exploratory role of play, but is also dialogic and procedural, inviting us to reflect on the human assumptions that shape technologies.\n
+`Play has traditionally been one way in which people have felt comfortable in performing speculation and transgressing their day-to-day roles. Interactivity can support the exploratory role of play, but is also dialogic and procedural, inviting us to reflect on the human assumptions that shape technologies.\n
 
 In our work, we draw these classes of designed experience together: We explore the terrain of critical and speculative design in a playful, interactive and participatory manner, demanding interactors to engage, to question, and to be present.\n
 
-Some of the more public activities that we engage in as part of this work that you should look out for are design jams and rapid prototyping sessions, casting calls, playtests, and exhibitions.`
+Some of the more public activities that we engage in as part of this work that you should look out for are design jams and rapid prototyping sessions, casting calls, playtests, and exhibitions.`]
 
 const About = () => {
 	const { nonsense } = useNonsense()
@@ -41,6 +41,12 @@ const About = () => {
 			<Page title="about">
 				<main className="page">
 					<article>
+						<div className="nn-grid">
+							<img
+								src={withPrefix('/img/IMG_6839.jpg')}
+								style={{ width: '100%' }}
+							/>
+						</div>
 						<Link to={firstPerson} className="nn-nav">
 							Who's here
 						</Link>
@@ -51,13 +57,7 @@ const About = () => {
 							Bibliography
 						</Link>
 						<h1>About</h1>
-						<div>{aboutText}</div>
-						<div className="nn-grid">
-							<img
-								src={withPrefix('/img/IMG_6839.jpg')}
-								style={{ width: '100%' }}
-							/>
-						</div>
+						<div>{aboutText[0] + aboutText[1]}</div>
 					</article>
 				</main>
 				<HUD />
@@ -70,12 +70,24 @@ const About = () => {
 			<main id="viewport">
 				<Canvas center={[0, 1.3, 0]} background={'#081529'}>
 					<Text
+						position={[-3, 0, 0]}
+						rotation={[0, 0.4, 0]}
 						maxWidth={4}
 						fontSize={0.2}
 						font={withPrefix('/fonts/UniNeue-HeavyItalic.woff')}
 					>
 						<meshNormalMaterial depthWrite={false} depthTest={false} side={2} />
-						{aboutText}
+						{aboutText[0]}
+					</Text>
+					<Text
+						position={[3, 0, 0]}
+						rotation={[0, -0.4, 0]}
+						maxWidth={4}
+						fontSize={0.2}
+						font={withPrefix('/fonts/UniNeue-HeavyItalic.woff')}
+					>
+						<meshNormalMaterial depthWrite={false} depthTest={false} side={2} />
+						{aboutText[1]}
 					</Text>
 					<AboutPage />
 				</Canvas>
